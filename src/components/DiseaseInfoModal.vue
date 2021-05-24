90f4d5746a602284fbf110f68b29ff67807373e1
Zohaib_Spk <zohaib.akram3536@gmail.com>
2021-05-25 02:51:02 +0500

    update diseasease

--- src/components/DiseaseInfoModal.vue
+++ src/components/DiseaseInfoModal.vue
@@ -11,6 +11,10 @@
           <b-row no-gutters>
             <b-col md="6">
               <b-card-img :src="disease.src" alt="Image" class="rounded-0" style="width:330px; height:260px"></b-card-img>
+              <b-button v-if="disease.description_audio" variant="outline-info" class="mt-2"
+                        @click="audio(disease.description_audio)">
+                <b-icon icon="volume-up" aria-hidden="true"></b-icon>
+              </b-button>
               <p class="urdu-font text-center px-5 pt-3 pb-5">{{disease.des}}</p>
             </b-col>
             <b-col md="6">
@@ -18,10 +22,18 @@
                 <b-card-text class="urdu-font text-center">
                   {{disease.symptoms}}
                 </b-card-text>
+                <b-button v-if="disease.symptoms_audio" variant="outline-info" class="mt-2" @click="audio(disease.symptoms_audio)">
+                  <b-icon icon="volume-up" aria-hidden="true"></b-icon>
+                </b-button>
               </b-card-body>
               <b-card-body title="علاج" style="text-align:right" v-if="disease.cure">
                 <b-card-text class="urdu-font text-center">
                   {{disease.cure}}
+                </b-card-text>
+              </b-card-body>
+              <b-card-body title="اثرات" style="text-align:right" v-if="disease.reasons">
+                <b-card-text class="urdu-font text-center">
+                  {{disease.reasons}}
                 </b-card-text>
               </b-card-body>
             </b-col>
@@ -42,6 +54,12 @@ export default {
 
   data() {
     return {}
+  },
+  methods:{
+    audio(audio) {
+      var sound = new Audio(audio);
+      sound.play();
+    }
   }
 }
 </script>
--- src/components/DiseasesCards.vue
+++ src/components/DiseasesCards.vue
@@ -1,20 +1,20 @@
 <template>
- <div>
+  <div>
     <b-card
-      :img-src="cartItem.src"
-      img-alt="Image"
-      img-top
-      img-height="160"
-      tag="article"
-      class="mb-5 card-anim shadow card-imgS"
+        :img-src="cartItem.src"
+        img-alt="Image"
+        img-top
+        img-height="160"
+        tag="article"
+        class="mb-5 card-anim shadow card-imgS"
     >
       <b-card-text>
         <b-row class="mb-1">
           <b-col class="inline">
-            <h4 class="urdu-font">{{cartItem.item}}</h4>
-<!--            <b-button variant="outline-info" class="mt-2" @click="audio(cartItem)">-->
-<!--              <b-icon icon="volume-up" aria-hidden="true"></b-icon>-->
-<!--            </b-button>-->
+            <h4 class="urdu-font">{{ cartItem.item }}</h4>
+            <!--            <b-button variant="outline-info" class="mt-2" @click="audio(cartItem)">-->
+            <!--              <b-icon icon="volume-up" aria-hidden="true"></b-icon>-->
+            <!--            </b-button>-->
           </b-col>
         </b-row>
         <b-row class="mt-3">
@@ -23,7 +23,9 @@
           </b-col>
         </b-row>
       </b-card-text>
-      <b-button :diseases="cartItem" variant="primary" class="w-100 urdu-font" @click="showModal = !showModal">بیماری کی وضاحت</b-button>
+      <b-button :diseases="cartItem" variant="primary" class="w-100 urdu-font" @click="showModal = !showModal">بیماری کی
+        وضاحت
+      </b-button>
       <DiseaseInfoModal :showModal="showModal" :disease="cartItem"/>
     </b-card>
   </div>
@@ -33,21 +35,21 @@
 import DiseaseInfoModal from './DiseaseInfoModal';
 
 export default {
-  components:{
+  components: {
     DiseaseInfoModal
   },
   props: {
-   cartItem:Object
+    cartItem: Object
   },
 
   data() {
-    return{
+    return {
       showModal: false
     }
   },
 
-  methods:{
-    audio(cartItem){
+  methods: {
+    audio(cartItem) {
       var sound = new Audio(cartItem.audio);
       sound.play();
     }
@@ -57,28 +59,28 @@ export default {
 
 
 <style scoped>
-  .card-anim{
-    width: 350px;
-    /*height: 400px;*/
-    padding: 15px 10px;
-    transition: 0.3s;
-  }
+.card-anim {
+  width: 350px;
+  /*height: 400px;*/
+  padding: 15px 10px;
+  transition: 0.3s;
+}
 
-  .card-anim:hover{
-    transform: scale(1.05);
-  }
+.card-anim:hover {
+  transform: scale(1.05);
+}
 
-  .des{
-    font-size: 18px;
-    letter-spacing: 0.05rem;
-    word-spacing: 0.6rem;
-  }
+.des {
+  font-size: 18px;
+  letter-spacing: 0.05rem;
+  word-spacing: 0.6rem;
+}
 
-  .inline{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
+.inline {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
 
 </style>
